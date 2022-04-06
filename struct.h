9cb5845f1fdf03c4c94e29f47b1be6bdf55dced1
Realnotema <100119630+Realnotema@users.noreply.github.com>
2022-04-06 17:53:28 +0300

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,1 +1,30 @@
+#ifndef STRUCT_H
+#define STRUCT_H
 
+typedef struct Item {
+        int pos;  
+        int size;
+} Item;
+
+typedef struct KeySpace1 {
+        Item key;
+        Item par;
+        Item busy;
+        Item info;
+} KeySpace1;
+
+typedef struct KeySpace2 {
+        Item key;
+        Item realise;
+        Item busy;
+        Item info;
+} KeySpace2;
+
+typedef struct Table {
+        KeySpace1 ks1;
+        KeySpace2 ks2;
+        Item msize;
+        Item csize;
+} Table;
+
+#endif
